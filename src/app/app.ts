f4eef42003158a7786f794b3cdd33aa27f2bac12
Robin Erike <robinerike@gmail.com>
2025-11-27 15:53:15 +0100

    g

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Signup],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
